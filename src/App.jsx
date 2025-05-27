dfdc2b5984ccf0ec7866c3c59267087f966a5d24
seohyeon-lee-2025 <seohyeonlee2020@gmail.com>
2025-05-28 06:37:23 +0900

    header css

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ function App() {
 	return (
 			<div>
 				<Header />
-				<h1>Student Sustainability Summit</h1>
-				<p>Bringing together students and professionals from around the world to showcase their work, connect with others in their areas of expertise, and discover life-changing sustainable solutions.</p>
-
-				<h2>Volunteering Opportunities</h2>
-
-				<h2>FAQs</h2>
+				<p>placeholder for content</p>
 				<Footer />
 			</div>
 	);
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,15 +9,15 @@ const Footer = () => {
 						<strong>Student Sustainability Summit</strong>
 						<nav>
 							<ul className='social-handles'>
-								<li><a href=""><img src="" alt="instagram"></img></a></li>
-								<li><a href=""><img src="" alt="facebook"></img></a></li>
-								<li><a href=""><img src="" alt="linkedin"></img></a></li>
-								<li><a href=""><img src="" alt="X"></img></a></li>
+								<li><a href=""><img src="src/assets/images/insta_logo.avif" alt="instagram" ></img></a></li>
+								<li><a href=""><img src="src/assets/images/facebook_logo.avif" alt="facebook"></img></a></li>
+								<li><a href=""><img src="src/assets/images/linkedin_logo.avif" alt="linkedin"></img></a></li>
+								<li><a href=""><img src="src/assets/images/x_logo.avif" alt="X"></img></a></li>
 							</ul>
 						</nav>
 					</div>
 
-					<div className='middle'>Placeholder in the middle</div>
+					<div className='middle'></div>
 
 					<div className='right-blocks'>
 						<span>Designed by UF Gator User Design</span>
